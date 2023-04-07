d322e803c84fe87a6e0a56930080dacacda6ca91
UsmanAziz113 <usman.aziz@taknik.com>
2023-04-07 20:53:48 +0500

    'Update code'

--- src/components/Layout/MyLayout.js
+++ src/components/Layout/MyLayout.js
@@ -1,47 +1,122 @@
-import { Layout, Menu } from 'antd';
+import React,{useState} from 'react'
+import { Layout, Menu } from 'antd'
 import {
   DesktopOutlined,
   PieChartOutlined,
   FileOutlined,
-} from '@ant-design/icons';
+} from '@ant-design/icons'
+import { Col, Row, Button } from 'antd'
 
-const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
+const { Header, Content, Footer, Sider } = Layout
+const { SubMenu } = Menu
 
 function MyLayout() {
-  return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible collapsedWidth="0">
-        <div className="logo" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+  const [selectedButton, setSelectedButton] = useState('Main');
+
+const handleButtonClick = (button) => {
+  setSelectedButton(button);
+};
+
+const renderMenuItems = () => {
+  switch (selectedButton) {
+    case 'Main':
+      return (
+        <>
           <Menu.Item key="1" icon={<PieChartOutlined />}>
             Option 1
           </Menu.Item>
           <Menu.Item key="2" icon={<DesktopOutlined />}>
             Option 2
           </Menu.Item>
-          <SubMenu key="sub1" icon={<FileOutlined />} title="Options">
-            <Menu.Item key="3">Option 3</Menu.Item>
-            <Menu.Item key="4">Option 4</Menu.Item>
-          </SubMenu>
-        </Menu>
-      </Sider>
+        </>
+      );
+    case 'Configure':
+      return (
+        <>
+          <Menu.Item key="3">Option 3</Menu.Item>
+          <Menu.Item key="4">Option 4</Menu.Item>
+        </>
+      );
+    default:
+      return null;
+  }
+};
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsedWidth={0}>
+      <div className="logo">
+        <img src="path/to/logo.png" alt="Logo" />
+      </div>
+      <Row justify="space-between" align="middle" gutter={[8]} >
+        <Col>
+          <Button
+            onClick={() => handleButtonClick('Main')}
+            type={selectedButton === 'Main' ? 'primary' : 'default'}
+            className="text-3xl "
+          >
+            Main
+          </Button>
+        </Col>
+        <Col>
+          <Button
+            onClick={() => handleButtonClick('Configure')}
+            type={selectedButton === 'Configure' ? 'primary' : 'default'}
+          >
+            Configure
+          </Button>
+        </Col>
+        <Col>
+          <Button
+            onClick={() => handleButtonClick('Tools')}
+            type={selectedButton === 'Tools' ? 'primary' : 'default'}
+          >
+            Tools
+          </Button>
+        </Col>
+        <Col>
+          <Button
+            onClick={() => handleButtonClick('Action')}
+            type={selectedButton === 'Action' ? 'primary' : 'default'}
+          >
+            Action
+          </Button>
+        </Col>
+      </Row>
+
+      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+        {renderMenuItems()}
+      </Menu>
+    </Sider>
+
       <Layout>
-        <Header className="site-layout-sub-header-background" style={{ padding: 0, textAlign: 'right' }}>
+        <Header
+          className="site-layout-sub-header-background"
+          style={{ padding: 0, textAlign: 'right' ,backgroundColor:'red'}}
+        >
           {/* Your header content goes here */}
         </Header>
         <Content style={{ margin: '0 16px', marginTop: 24 }}>
-          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
             {/* Your page content goes here */}
           </div>
         </Content>
-       
       </Layout>
-      <Footer style={{ textAlign: 'center', position: 'absolute', bottom: 0, width: '100%',backgroundColor:'wheat' }}>
-          {/* Your footer content goes here */}
-        </Footer>
+      <Footer
+        style={{
+          textAlign: 'center',
+          position: 'absolute',
+          bottom: 0,
+          width: '100%',
+          backgroundColor: 'wheat',
+        }}
+      >
+        {/* Your footer content goes here */}
+      </Footer>
     </Layout>
-  );
+  )
 }
 
-export default MyLayout;
+export default MyLayout
